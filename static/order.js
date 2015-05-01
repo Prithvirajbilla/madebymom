6236114f0a00823026f717c8a3423b2d6a8d0825
Prithviraj Billa <prithviraj.m.billa@gmail.com>
2015-05-02 02:29:32 +0530

    cart adding working

--- static/order.js
+++ static/order.js
@@ -1,3 +1,38 @@
-simpleCart.bind( 'beforeAdd' , function( item ){
-	console.log( item.get('name') );
-});
+$(".btn-number").click(
+		function  () {
+			var type_of_button = $(this).data("type");
+			var quant = $(this).data("number");
+			var id = "#"+$(this).data("id");
+			if(type_of_button == "minus")
+			{
+				quant -= 1;
+				if(quant < 1)
+				{
+					quant = 0;
+					$(id+"  .rating-box").hide()
+				}
+				else
+				{
+
+					$(id+" .rating-box").show();
+					$(id+ " .rating-box").text(quant);
+					$(id+ " .btn-number").data("number",quant);
+				}
+			}
+			else if(type_of_button == "plus")
+			{
+				quant+=1;
+				if(quant > 10)
+				{
+					//
+				}
+				else
+				{
+					$(id+ " .rating-box").show();
+					$(id+ " .rating-box").text(quant);
+					$(id+ " .btn-number").data("number",quant);
+				}
+			}
+		}
+
+	)
