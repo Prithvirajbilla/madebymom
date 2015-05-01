56f8724c8f858bb14416bac176ce8c5e37f43c54
Prithviraj Billa <prithviraj.m.billa@gmail.com>
2015-05-02 03:39:25 +0530

    added cart feature

--- static/order.js
+++ static/order.js
@@ -1,3 +1,6 @@
+var items_count = 0;
+
+
 $(".btn-number").click(
 		function  () {
 			var type_of_button = $(this).data("type");
@@ -7,12 +10,29 @@ $(".btn-number").click(
 			{
 				if(quant == 10)
 					$(id+" .btn-number[data-type='plus']").removeAttr("disabled");
-				
+
 				quant -= 1;
+				if(quant == 0)
+				{
+					items_count-=1;
+					if(items_count <= 0)
+					{
+						items_count=0;
+						$("#cart_count").hide();
+					}
+					else
+					{
+						$("#cart_count").show();
+						$("#cart_count").text(items_count);
+					}
+				}
+
 				if(quant < 1)
 				{
 					quant = 0;
 					$(id+"  .rating-box").hide()
+					$(id+ " .btn-number").data("number",quant);
+					$(id+" .btn-number[data-type='plus']").data("number",quant);
 				}
 				else
 				{
@@ -24,6 +44,14 @@ $(".btn-number").click(
 			}
 			else if(type_of_button == "plus")
 			{
+
+				if(quant == 0)
+				{
+					items_count+=1;
+					$("#cart_count").show();
+					$("#cart_count").text(items_count);
+				}
+
 				quant+=1;
 				if(quant > 10)
 				{
