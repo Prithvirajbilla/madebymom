56d9f71d65207bbcfbf4f7aa449a51298e3abe6f
Prithviraj Billa <prithviraj.m.billa@gmail.com>
2015-05-02 23:48:39 +0530

    added things

--- static/cart.js
+++ static/cart.js
@@ -41,3 +41,30 @@ function()
 	$.cookie('cart_basket',JSON.stringify(cart_basket));
 }
 );
+
+
+$(".quantity").on("input",
+	function ()
+	{
+		var id = $(this).data("id");
+		var txt = $(this).val();
+		if(txt >=  10)
+		{
+			txt = 10;
+			//changing the price
+			$.notify("You cannot order more than 10 items of the same type!.To Bulk order, Contact us", "warn");
+			$(this).val(txt);
+			var cart_basket = JSON.parse($.cookie("cart_basket"));
+			cart_basket["#"+id] = txt;
+			$.cookie("cart_basket",JSON.stringify(cart_basket));
+		}
+		console.log(id);
+		var price = $("#"+id+"-table .price").text();
+		price = parseInt(price.substring(2));
+		$("#"+id+"-table .total_price").text("₹ "+price*txt);
+
+		console.log(txt);
+
+		find_total();
+	}
+);
